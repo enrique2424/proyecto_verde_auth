4f54ea713957c2ae29ce7f919868c6ddb3975d13
nery.carrizales <nery.carrizales@bg.com.bo>
2025-01-15 17:27:14 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+//import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
@@ -8,14 +8,14 @@ async function main() {
     origin: process.env.CORS.split(','),
   });
   // Configuración de Swagger
-  const config = new DocumentBuilder()
+  /*const config = new DocumentBuilder()
     .setTitle('API Documentación')
     .setDescription('Documentación de la API con Swagger')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // El endpoint de Swagger será /api
+  SwaggerModule.setup('api', app, document);*/
 
   await app.listen(process.env.PORT);
 }
